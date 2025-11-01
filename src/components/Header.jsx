496371f8192be6aa7d98ddee93e7024ce186943d
Mario Fransiskus Sitepu <stinart123@GMAIL.COM>
2025-11-01 14:50:03 +0700

    feat : adding web logo and change web theme into yellow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import logo from '../makan.png';
 
 /**
  * Header Component
- * Komponen header aplikasi dengan judul utama
+ * Komponen header aplikasi dengan logo dan judul utama
  */
 const Header = () => {
   return (
     <header className="app-header">
-      <h1>Recipe Finder</h1>
+      <div className="header-content">
+        <img src={logo} alt="Recipe Finder Logo" className="header-logo" />
+        <h1>Recipe Finder</h1>
+      </div>
     </header>
   );
 };
